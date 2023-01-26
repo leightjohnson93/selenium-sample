3c91c39c0b7945083db0c3286e6d15cca1fc5a0f
Leight Johnson <leightjohnson93@gmail.com>
2023-01-26 15:11:36 -0700

    increase timeout to start chrome

--- step_definitions/steps.ts
+++ step_definitions/steps.ts
@@ -6,13 +6,8 @@ let driver: ThenableWebDriver;
 const password = "secret_sauce";
 
 BeforeAll(
-  () =>
-    (driver = new Builder()
-      .withCapabilities({
-        chromeOptions: { args: ["--no-sandbox", "--disable-dev-shm-usage"] },
-      })
-      .forBrowser("chrome")
-      .build())
+  { timeout: 60 * 1000 },
+  () => (driver = new Builder().forBrowser("chrome").build())
 );
 
 Given("I am on the website {string}", async (url: string) => {
